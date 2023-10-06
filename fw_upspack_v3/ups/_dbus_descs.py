b93de12c8e8bacd9fe17b5ee9613c31ceaeeac3e
Roberto Pompermaier <robypomper@gmail.com>
2023-10-06 15:34:12 +0200

    feat(ALL): Customized for UPS Pack V3 values

--- fw_upspack_v3/ups/_dbus_descs.py
+++ fw_upspack_v3/ups/_dbus_descs.py
@@ -1,104 +1,20 @@
 #!/usr/bin/python3
 
-# List of supported devices' types
-DEV_IFACE_BMV = "com.victron.BMV"
-DEV_IFACE_BlueSolar_MPPT = "com.victron.BlueSolarMPPT"
-DEV_IFACE_BlueSolar_MPPT_VECan = "com.victron.BlueSolarMPPTVECan"
-DEV_IFACE_SmartSolar_MPPT = "com.victron.SmartSolarMPPT"
-DEV_IFACE_SmartSolar_MPPT_VECan = "com.victron.SmartSolarMPPTVECan"
-DEV_IFACE_Phoenix_Inverter = "com.victron.PhoenixInverter"
-DEV_IFACE_Phoenix_Smart_IP43_Charger = "com.victron.PhoenixSmartIP43Charger"
-DEV_IFACE_SmartShunt = "com.victron.SmartShunt"
+# List of supported DBus objects' definitions
+# Strings used as default value to populate the PID dict
 
-# List of all DBus objects' definitions
-# Defined using the specs at https://www.victronenergy.com/upload/documents/VE.Direct-Protocol-3.33.pdf
-
-DEV_DBUS_DESC_SmartSolar_MPPT = '''
-<node>
+DEV_DBUS_DESC_UPSSmart = '''<node>
   <interface name='{dbus_iface}'>
-    <property name="load_state" type="b" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="serial_number" type="s" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="battery_current" type="d" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="panel_power" type="d" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="state_operation" type="s" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="tracker_operation_mode" type="s" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="off_reason" type="s" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="error_code" type="s" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="load_current" type="d" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="yield_total" type="d" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="yield_today" type="d" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="max_power_today" type="d" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="yield_yesterday" type="d" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="max_power_yesterday" type="d" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="day_sequence_number" type="i" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="product_id" type="s" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
     <property name="firmware_version" type="s" access="read">
       <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
     </property>
-    <property name="battery_voltage" type="d" access="read">
+    <property name="state_operation" type="b" access="read">
       <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
     </property>
-    <property name="panel_voltage" type="i" access="read">
+    <property name="battery_capacity" type="d" access="read">
       <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
     </property>
-    
-    <property name="battery_voltage_percent" type="d" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="battery_voltage_min" type="d" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="battery_voltage_max" type="d" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="load_voltage" type="d" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="load_power" type="d" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="load_power_percent" type="d" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="load_power_max" type="d" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="panel_power_percent" type="d" access="read">
-      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
-    </property>
-    <property name="panel_power_max" type="d" access="read">
+    <property name="voltage_out" type="d" access="read">
       <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
     </property>
     
--- fw_upspack_v3/ups/simulator.py
+++ fw_upspack_v3/ups/simulator.py
@@ -1,68 +1,33 @@
 #!/usr/bin/python3
 
-import random
-
-from .device import VEDevice
+from .device import Device
 from .mappings import *
+from ..commons import regenerateValue
 
-class VEDeviceSimulator(VEDevice):
 
-    def __init__(self, device: str = '/dev/ttyUSB0', speed: int = 19200):
+class DeviceSimulator(Device):
+
+    def __init__(self, device: str = '/dev/ttyAMA0', speed: int = 9600):
         super().__init__(device, speed, False)
         self._data = {
-            'FW': '',
-            'SER#': '',
-            'V': "12000.0",
-            'I': '0.0',
-            'VPV': '0',
-            'PPV': '0',
-            'CS': '0',
-            'MPPT': '0',
-            'OR': '0',
-            'ERR': '0',
-            'LOAD': "OFF",
-            'IL': '0',
-            'H19': '0',
-            'H20': '0',
-            'H21': '0',
-            'H22': '0',
-            'H23': '0',
-            'HSDS': '0',
-            'PID': '0xA060'
+            'SmartUPS': 'V3.2P',
+            'Vin': 'GOOD',
+            'BATCAP': "100",
+            'Vout': '5250'
         }
 
     def refresh(self, reset_data=False) -> bool:
         self._data = {
-            'FW': '161',
-            'SER#': 'HQ221234567',
-            'V': max(min(self.regenerateValue(self._data['V'], 200), 15000), 10000),
-            'I': max(min(self.regenerateValue(self._data['I'], 200), 4000), 0),
-            'VPV': max(min(self.regenerateValue(self._data['VPV'], 200), 15000), 0),
-            'PPV': max(min(self.regenerateValue(self._data['PPV'], 5), 200), 0),
-            'CS': '0',
-            'MPPT': '0',
-            'OR': '0x00000001',
-            'ERR': '0',
-            'LOAD': "ON" if random.randint(0, 1) else "OFF",
-            'IL': max(min(self.regenerateValue(self._data['IL'], 200), 4000), 0),
-            'H19': '230',
-            'H20': '0',
-            'H21': '0',
-            'H22': '0',
-            'H23': '0',
-            'HSDS': '25',
-            'PID': '0xA060'
+            'SmartUPS': 'V3.2P',
+            'Vin': 'GOOD' if random.randint(0, 1) else "NG",
+            'BATCAP': max(min(regenerateValue(self._data['BATCAP'], 1), 100), 0),
+            'Vout': max(min(regenerateValue(self._data['Vout'], 10), 5400), 5100),
         }
         return True
 
-    @staticmethod
-    def regenerateValue(str_value, range_value):
-        prev_value = props_parser_float(str_value)
-        inc = random.randint(0, range_value) - (range_value / 2)
-        return prev_value + inc
-
 
 if __name__ == '__main__':
-    v = VEDeviceSimulator()
-    print("{}/{}# [{}CONNECTED]: {}".format(v.device_model, v.device_serial,
-                                            "" if v.is_connected else "NOT ", v.latest_data["V"]))
+    v = DeviceSimulator()
+    print("{}# [{}CONNECTED]: {}".format(v.device_type,
+                                         "" if v.is_connected else "NOT ",
+                                         v.latest_data["V"]))
