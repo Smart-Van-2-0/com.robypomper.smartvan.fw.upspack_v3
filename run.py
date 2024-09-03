7fa9f77e22c3eeee4a2ce20eb67111108ffb30dc
Roberto Pompermaier <robypomper@gmail.com>
2024-09-03 15:17:04 +0200

    fix(Device): Fixed wrong HALT signal handling
    Run: removed must_shutdown because replaced by Device::must_terminate
    Run: removed _register_kill_signals() and __handle_kill_signals(...) from run.py because moved to device.py
    DeviceSerial: removed must_terminate and terminate() because moved to device.py
    Device: added must_terminate, _register_kill_signals() and __handle_kill_signals(...)

--- run.py
+++ run.py
@@ -59,7 +59,6 @@ EXIT_INIT_DBUS = 2
 # default logger, used if _setup_logging() was not called
 logger = logging.getLogger()
 dev_global: Optional[Device] = None
-must_shutdown = False
 properties_cache = {}
 
 
@@ -152,7 +151,7 @@ def _init_logging(dev, debug, quiet):
 def _init_device(port, speed, wait_connection=True, simulate_dev=False) -> Device:
     """ Init and configure Device. """
 
-    global must_shutdown, dev_global
+    global dev_global
 
     if simulate_dev:
         logger.debug("Simulate device")
@@ -163,22 +162,23 @@ def _init_device(port, speed, wait_connection=True, simulate_dev=False) -> Devic
     logger.debug("Read first data from device...")
     dev.refresh()
 
-    if must_shutdown:
+    if dev.must_terminate:
         logger.warning("Received terminate signal during Device initialization, exit.")
     elif not dev.is_connected and wait_connection:
         logger.warning("Device not available, retry in {} seconds. Press (Ctrl+C) to exit.".format(CONN_RETRY))
         try:
-            must_shutdown = False
-            while not dev.is_connected and not must_shutdown:
+            while not dev.is_connected and not dev.must_terminate:
                 time.sleep(CONN_RETRY)
                 dev.refresh()
-                if not dev.is_connected:
+                if not dev.is_connected and not dev.must_terminate:
                     logger.debug("Device still not available, retry in {} seconds.".format(CONN_RETRY))
         except KeyboardInterrupt:
             logger.info("Terminating required by the user.")
             exit(EXIT_INIT_TERMINATED)
 
-    if dev.is_connected:
+    if dev.must_terminate:
+        logger.warning("Terminating required by the user")
+    elif dev.is_connected:
         logger.info("Connected to Device '{}'.".format(dev.device_pid))
     else:
         logger.info("Initialized Device, but not connected.")
@@ -199,9 +199,9 @@ def _init_dbus_object(dbus_name, dev_id, dbus_obj_path, dbus_iface) -> DBusObjec
 
 
 def _publish_dbus_object(dbus, dbus_obj):
-    global must_shutdown
+    global dev_global
 
-    while not must_shutdown:
+    while not dev_global.must_terminate:
         try:
             dbus_obj.publish(dbus)
             break
@@ -213,19 +213,18 @@ def _publish_dbus_object(dbus, dbus_obj):
             else:
                 raise RuntimeError("Can't publish the object on DBus") from err
 
-    if must_shutdown:
+    if dev_global.must_terminate:
         logger.warning("Received terminate signal during Object publication on DBus, exit.")
 
 
 def _main_loop(dev, dbus_obj, development=False):
     """ Current script's main loop. """
 
-    global must_shutdown
+    global dev_global
 
     # Main thread loop
     logger.info("Start {} Main Loop. Press (Ctrl+C) to quit.".format(FW_NAME))
-    must_shutdown = False
-    while not must_shutdown:
+    while not dev_global.must_terminate:
         logger.info("  ==== ==== ==== ====")
         logger.debug("Start fetch/pull device")
 
@@ -242,7 +241,7 @@ def _main_loop(dev, dbus_obj, development=False):
 
         except KeyboardInterrupt:
             logger.info("Terminating required by the user.")
-            must_shutdown = True
+            dev_global.terminate()
         except Exception as unknown_error:
             logger.error("Unknown error on Main Loop: {}, retry later".format(unknown_error))
             if development is True:
@@ -254,13 +253,13 @@ def _main_loop(dev, dbus_obj, development=False):
         sleepTime = LOOP_SLEEP if dev.is_connected else CONN_RETRY
         try:
             for i in range(sleepTime):
-                if must_shutdown:
+                if dev_global.must_terminate:
                     break
                 time.sleep(1)
 
         except KeyboardInterrupt:
             logger.info("Terminating required by the user.")
-            must_shutdown = True
+            dev_global.terminate()
 
     logger.info(FW_NAME + " Main Loop terminated.")
 
@@ -355,32 +354,14 @@ def _update_property_derivatives(dbus_obj, property_name, development=False):
                 traceback.print_exc()
 
 
-def _register_kill_signals():
-    signal.signal(signal.SIGINT, __handle_kill_signals)
-    signal.signal(signal.SIGTERM, __handle_kill_signals)
-
-
-def __handle_kill_signals(signo, _stack_frame):
-    global must_shutdown
-
-    logger.info("Received `{}` signal. Shutting down...".format(signo))
-    # SIGINT    2   <= Ctrl+C
-    # SIGTERM   15  <= kill PID
-    if dev_global is not None:
-        dev_global.terminate()
-    must_shutdown = True
-
-
 def main(port, speed, dbus_name, obj_path=None, dbus_iface=None, simulate_dev=False, development=False):
     """ Initialize a Device to read data and a DBus Object to share collected data. """
     global dev_global
 
-    _register_kill_signals()
-
     # Init Device
     try:
         dev_global = _init_device(port, speed, True, simulate_dev)
-        if not dev_global.is_connected and must_shutdown:
+        if not dev_global.is_connected and dev_global.must_terminate:
             exit(0)
         if dev_global.device_type_code == "":
             logger.warning("Device not recognized, exit.")
